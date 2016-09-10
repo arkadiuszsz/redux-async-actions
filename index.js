1c69bcbfad8e4d0bb54c9d6515ec22125ee52d0a
David Kearns <dave.p.kearns@gmail.com>
2016-09-10 02:40:16 -0700

    Compile to ES5 on publish

--- index.js
+++
@@ -1,4 +0,0 @@
-import { createAsyncAction } from './async-actions.js';
-import asyncActionMiddleware from './middleware.js';
-
-export { createAsyncAction, asyncActionMiddleware };
---
+++ src/index.js
@@ -0,0 +1,4 @@
+import { createAsyncAction } from './async-actions.js';
+import asyncActionsMiddleware from './middleware.js';
+
+export { createAsyncAction, asyncActionsMiddleware };
